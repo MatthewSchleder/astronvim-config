163ffc71b35ffe98f361907eb725f5d118751d6f
Matthew Schleder <schledermatthew@gmail.com>
2023-02-14 14:05:47 -0500

    Update polish.lua

--- polish.lua
+++ polish.lua
@@ -4,6 +4,8 @@
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
 return function()
+  local current_win = vim.fn.win_getid()
+  print(current_win)
   vim.api.nvim_create_augroup("neotree", {})
     vim.api.nvim_create_autocmd("UiEnter", {
       desc = "Open Neotree automatically",
@@ -11,6 +13,9 @@ return function()
       callback = function()
         if vim.fn.argc() == 0 then
           vim.cmd "Neotree toggle"
+          local neotree_win = vim.fn.win_getid()
+          print(neotree_win)
+          vim.api.nvim_set_current_win(current_win)
         end
       end,
     })
